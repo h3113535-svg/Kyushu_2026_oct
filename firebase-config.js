940a4d134f3f317d2a66344e953a00d73c820798
h3113535-svg <h3113535@gmail.com>
2026-08-25 01:33:57 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
 /* Firebase Web config is public by design. Do not put passwords or service-account keys here. */
 window.KYUSHU_FIREBASE_CONFIG = {
-  apiKey: "PASTE_API_KEY_HERE",
+  apiKey: "AIzaSyAnyC1Rpp6gvPXeeAkkXHpLX_uYCoJ2DJw",
   authDomain: "kyushu2026-9b6b9.firebaseapp.com",
   databaseURL: "https://kyushu2026-9b6b9-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "kyushu2026-9b6b9",
-  appId: "PASTE_APP_ID_HERE"
+  appId: "1:723262823396:web:927c26360a1611b7c245f1"
 };
 window.KYUSHU_PRIVATE_PATH = "trips/kyushu-oct-2026";
